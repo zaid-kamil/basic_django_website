29e29d9317763f55161814e55a5511b68527e341
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-04-05 18:12:34 +0530

    student ka din

--- CORE/settings.py
+++ CORE/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATIC_ROOT = BASE_DIR.joinpath('static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 
