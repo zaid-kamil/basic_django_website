7575ecbc22368f4b77949d2285ef349a361c6e6a
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-04-07 18:27:22 +0530

    display

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_list, name='index'),
+    path('', views.blog_list, name='blogs'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Article, Tag
 
 def blog_list(request):
     # load all blog articles from database
-    return render(request, 'blog/list.html', {})
+    articles = Article.objects.filter().order_by('-created_on')
+    ctx = {
+        'articles': articles,
+    }
+    return render(request, 'blog/list.html', ctx)
